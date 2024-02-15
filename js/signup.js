fe6bbd33bcc28e98b089aad4c2b2a6d6c7bcfa2f
Luana Magalhães <123824836+lU4N4ht@users.noreply.github.com>
2024-02-15 11:49:50 -0300

    Iniciando o cadastro

---
+++ js/signup.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const btnLogin = document.getElementById('signup');
+
+async function userSignup(){
+
+    const inputName = document.getElementById('name').value;
+    const inputEmail = document.getElementById('email').value;
+    const inputPassword = document.getElementById('password').value;
+    const inputConfirm = document.getElementById('confirm-password').value;
+}
